f065fe0147b4e7574681a33660b816433ec91495
Omoshlawi <lawiomosh3@gmail.com>
2024-01-25 12:59:45 +0300

    Added profile update and tested with success results

--- src/features/users/routes/index.ts
+++ src/features/users/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getUser, getUsers, viewProfile } from "../controlers";
+import { getUser, getUsers, updateProfile, viewProfile } from "../controlers";
 import { requireAuthenticated } from "../../../middlewares";
 
 const router = Router();
 router.get("/", getUsers);
 router.get("/profile", requireAuthenticated, viewProfile);
+router.post("/profile", requireAuthenticated, updateProfile);
 router.get("/:id", getUser);
 export default router;
